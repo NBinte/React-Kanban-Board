abf703c722922adae6026fb2ea7c9a763f59086b
NBinte <nuzhatbintei@gmail.com>
2022-08-10 02:18:43 +0600

    Create board

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import FormComp from "./FormComp";
+import Card from "react-bootstrap/Card";
 
 import style from "../Assets/css/App.module.css";
 
@@ -19,6 +20,45 @@ const App = () => {
             <Container fluid className={style.app}>
                 <Row>
                     <FormComp></FormComp>
+                </Row>
+
+                <Row className={style.boardRow}>
+                    <Col className={style.boardColumn}>
+                        <h3 className={style.columnHeader}>To Do</h3>
+                        <div className={style.columnData}>
+                            <Card className={style.cardStyle}>
+                                <Card.Body>Task 01</Card.Body>
+                            </Card>
+
+                            <Card className={style.cardStyle}>
+                                <Card.Body>Task 02</Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+                    <Col className={style.boardColumn}>
+                        <h3 className={style.columnHeader}>In Progress</h3>
+                        <div className={style.columnData}>
+                            <Card className={style.cardStyle}>
+                                <Card.Body>Task 04</Card.Body>
+                            </Card>
+
+                            <Card className={style.cardStyle}>
+                                <Card.Body>Task 03</Card.Body>
+                            </Card>
+
+                            <Card className={style.cardStyle}>
+                                <Card.Body>Task 05</Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+                    <Col className={style.boardColumn}>
+                        <h3 className={style.columnHeader}>Done</h3>
+                        <div className={style.columnData}>
+                            <Card className={style.cardStyle}>
+                                <Card.Body>Task 02</Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
                 </Row>
             </Container>
         </>
