955b53e52ddad76643523c2b9b168b3999fedb6e
NBinte <nuzhatbintei@gmail.com>
2022-08-12 00:00:21 +0600

    Complete other drag and drop events

--- src/Components/App.js
+++ src/Components/App.js
@@ -17,23 +17,16 @@ const App = () => {
         { name: "task5", category: "progress" },
         { name: "task6", category: "done" }
     ]);
-    console.log(tasks);
 
     const handleOnDragStart = (e, id) => {
-        console.log("dragstart " + id);
-
         e.dataTransfer.setData("id", id);
     };
 
     const handleOnDragOver = e => {
         e.preventDefault();
-
-        console.log("dragover " + e);
     };
 
     const handleOnDrop = (e, category) => {
-        console.log("ondrop " + e);
-
         let taskId = e.dataTransfer.getData("id");
 
         let filteredTasks = tasks.filter((eachTask, taskIndex) => {
@@ -62,15 +55,23 @@ const App = () => {
                 <Row className={style.boardRow}>
                     <Col className={style.boardColumn}>
                         <h3 className={style.columnHeader}>To Do</h3>
-                        <div className={style.columnData}>
+                        <div
+                            className={style.columnData}
+                            onDragOver={e => {
+                                handleOnDragOver(e);
+                            }}
+                            onDrop={e => {
+                                handleOnDrop(e, "todo");
+                            }}
+                        >
                             {tasks.map((eachTask, taskIndex) => {
                                 if (eachTask.category == "todo") {
                                     let id = eachTask.name;
                                     return (
                                         <Card
-                                            draggable='true'
                                             className={style.cardStyle}
                                             key={taskIndex}
+                                            draggable='true'
                                             onDragStart={e => {
                                                 handleOnDragStart(e, id);
                                             }}
@@ -95,14 +96,15 @@ const App = () => {
                         >
                             {tasks.map((eachTask, taskIndex) => {
                                 if (eachTask.category == "progress") {
+                                    let id = eachTask.name;
                                     return (
                                         <Card
-                                            draggable='true'
                                             className={style.cardStyle}
                                             key={taskIndex}
-                                            // onDragStart={e => {
-                                            //     handleOnDragStart(e, eachTask.name);
-                                            // }}
+                                            draggable='true'
+                                            onDragStart={e => {
+                                                handleOnDragStart(e, id);
+                                            }}
                                         >
                                             <Card.Body>{eachTask.name}</Card.Body>
                                         </Card>
@@ -113,17 +115,26 @@ const App = () => {
                     </Col>
                     <Col className={style.boardColumn}>
                         <h3 className={style.columnHeader}>Done</h3>
-                        <div className={style.columnData}>
+                        <div
+                            className={style.columnData}
+                            onDragOver={e => {
+                                handleOnDragOver(e);
+                            }}
+                            onDrop={e => {
+                                handleOnDrop(e, "done");
+                            }}
+                        >
                             {tasks.map((eachTask, taskIndex) => {
                                 if (eachTask.category == "done") {
+                                    let id = eachTask.name;
                                     return (
                                         <Card
-                                            draggable='true'
                                             className={style.cardStyle}
                                             key={taskIndex}
-                                            // onDragStart={e => {
-                                            //     handleOnDragStart(e, eachTask.name);
-                                            // }}
+                                            draggable='true'
+                                            onDragStart={e => {
+                                                handleOnDragStart(e, id);
+                                            }}
                                         >
                                             <Card.Body>{eachTask.name}</Card.Body>
                                         </Card>
